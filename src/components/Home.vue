4e08a3373562cc281eca9bf591dee4fe8c6ec1aa
travelwor1d <mobilexapp190408@gmail.com>
2021-06-04 17:16:30 -0500

    selector orientation

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,6 +9,8 @@
           :components="{Deselect, OpenIndicator}"
           :options="countries"
           :searchable="isMobile ? false : true"
+          :calculate-position="withPopper"
+          append-to-body 
           v-model="countrySelected"
           label="name"
           class="selectItems__select"
@@ -27,6 +29,8 @@
           :options="years"
           :disabled="countrySelected ? false : true"
           :searchable="isMobile ? false : true"
+          :calculate-position="withPopper"
+          append-to-body 
           ref="yearSelect"
           class="selectItems__select"
           placeholder="(type or select)"
@@ -34,7 +38,7 @@
         </v-select>
       </v-col>
     </v-row>
-    <v-row>
+    <v-row class="bottom-class">
       <!-- 3.Countries -->
       <v-col cols="12">
         Countries (Default: United State)
@@ -42,6 +46,8 @@
           :components="{Deselect, OpenIndicator}"
           :options="countries"
           :searchable="isMobile ? false : true"
+          :calculate-position="withPopper"
+          append-to-body 
           v-model="defaultCountry"
           label="name"
           class="selectItems__select"
@@ -52,190 +58,6 @@
           </template>
         </v-select>
       </v-col>
-    </v-row>
-    <v-row>
-      <!-- 3.Countries -->
-      <v-col cols="12">
-        Countries (Default: United State)
-        <v-select
-          :components="{Deselect, OpenIndicator}"
-          :options="countries"
-          :searchable="isMobile ? false : true"
-          v-model="defaultCountry"
-          label="name"
-          class="selectItems__select"
-          placeholder="(type or select)"
-        >
-          <template v-slot:option="country">
-            {{ country.name }}
-          </template>
-        </v-select>
-      </v-col>
-    </v-row>
-    <v-row>
-      <!-- 3.Countries -->
-      <v-col cols="12">
-        Countries (Default: United State)
-        <v-select
-          :components="{Deselect, OpenIndicator}"
-          :options="countries"
-          :searchable="isMobile ? false : true"
-          v-model="defaultCountry"
-          label="name"
-          class="selectItems__select"
-          placeholder="(type or select)"
-        >
-          <template v-slot:option="country">
-            {{ country.name }}
-          </template>
-        </v-select>
-      </v-col>
-    </v-row>
-    <v-row>
-      <!-- 3.Countries -->
-      <v-col cols="12">
-        Countries (Default: United State)
-        <v-select
-          :components="{Deselect, OpenIndicator}"
-          :options="countries"
-          :searchable="isMobile ? false : true"
-          v-model="defaultCountry"
-          label="name"
-          class="selectItems__select"
-          placeholder="(type or select)"
-        >
-          <template v-slot:option="country">
-            {{ country.name }}
-          </template>
-        </v-select>
-      </v-col>
-    </v-row>
-    <v-row>
-      <!-- 3.Countries -->
-      <v-col cols="12">
-        Countries (Default: United State)
-        <v-select
-          :components="{Deselect, OpenIndicator}"
-          :options="countries"
-          :searchable="isMobile ? false : true"
-          v-model="defaultCountry"
-          label="name"
-          class="selectItems__select"
-          placeholder="(type or select)"
-        >
-          <template v-slot:option="country">
-            {{ country.name }}
-          </template>
-        </v-select>
-      </v-col>
-    </v-row>
-    <v-row>
-      <!-- 3.Countries -->
-      <v-col cols="12">
-        Countries (Default: United State)
-        <v-select
-          :components="{Deselect, OpenIndicator}"
-          :options="countries"
-          :searchable="isMobile ? false : true"
-          v-model="defaultCountry"
-          label="name"
-          class="selectItems__select"
-          placeholder="(type or select)"
-        >
-          <template v-slot:option="country">
-            {{ country.name }}
-          </template>
-        </v-select>
-      </v-col>
-    </v-row>
-    <v-row>
-      <!-- 3.Countries -->
-      <v-col cols="12">
-        Countries (Default: United State)
-        <v-select
-          :components="{Deselect, OpenIndicator}"
-          :options="countries"
-          :searchable="isMobile ? false : true"
-          v-model="defaultCountry"
-          label="name"
-          class="selectItems__select"
-          placeholder="(type or select)"
-        >
-          <template v-slot:option="country">
-            {{ country.name }}
-          </template>
-        </v-select>
-      </v-col>
-    </v-row>
-    <v-row>
-      <!-- 3.Countries -->
-      <v-col cols="12">
-        Countries (Default: United State)
-        <v-select
-          :components="{Deselect, OpenIndicator}"
-          :options="countries"
-          :searchable="isMobile ? false : true"
-          v-model="defaultCountry"
-          label="name"
-          class="selectItems__select"
-          placeholder="(type or select)"
-        >
-          <template v-slot:option="country">
-            {{ country.name }}
-          </template>
-        </v-select>
-      </v-col>
-    </v-row>
-    <v-row>
-      <!-- 3.Countries -->
-      <v-col cols="12">
-        Countries (Default: United State)
-        <v-select
-          :components="{Deselect, OpenIndicator}"
-          :options="countries"
-          :searchable="isMobile ? false : true"
-          v-model="defaultCountry"
-          label="name"
-          class="selectItems__select"
-          placeholder="(type or select)"
-        >
-          <template v-slot:option="country">
-            {{ country.name }}
-          </template>
-        </v-select>
-      </v-col>
-    </v-row>
-    <v-row>
-      <!-- 3.Countries -->
-      <v-col cols="12">
-        Countries (Default: United State)
-        <v-select
-          :components="{Deselect, OpenIndicator}"
-          :options="countries"
-          :searchable="isMobile ? false : true"
-          v-model="defaultCountry"
-          label="name"
-          class="selectItems__select"
-          placeholder="(type or select)"
-        >
-          <template v-slot:option="country">
-            {{ country.name }}
-          </template>
-        </v-select>
-      </v-col>
-    </v-row>
-    <v-row>
-      <select>
-        <option value="">2</option>
-        <option value="">2</option>
-        <option value="">2</option>
-        <option value="">2</option>
-        <option value="">2</option>
-        <option value="">2</option>
-        <option value="">2</option>
-        <option value="">2</option>
-        <option value="">2</option>
-      </select>
     </v-row>
   </v-container>
 </template>
@@ -244,6 +66,7 @@
 import Indicator from './Indicator';
 import axios from 'axios';
 import { isMobile } from 'mobile-device-detect';
+import { createPopper } from '@popperjs/core';
 
 export default {
   name: "Home",
@@ -257,7 +80,8 @@ export default {
       countrySelected: "",
       defaultCountry: "",
       years: [],
-      isMobile: isMobile
+      isMobile: isMobile,
+      placement: "bottom"
     };
   },
   
@@ -289,6 +113,50 @@ export default {
           console.log(error);
         })
     },
+  },
+
+  methods: {
+    withPopper (dropdownList, component, {width}) {
+      /**
+       * We need to explicitly define the dropdown width since
+       * it is usually inherited from the parent with CSS.
+       */
+      dropdownList.style.width = width;
+
+      /**
+       * Here we position the dropdownList relative to the $refs.toggle Element.
+       *
+       * The 'offset' modifier aligns the dropdown so that the $refs.toggle and
+       * the dropdownList overlap by 1 pixel.
+       *
+       * The 'toggleClass' modifier adds a 'drop-up' class to the Vue Select
+       * wrapper so that we can set some styles for when the dropdown is placed
+       * above.
+       */
+      const popper = createPopper(component.$refs.toggle, dropdownList, {
+        placement: this.placement,
+        modifiers: [
+          {
+            name: 'offset', options: {
+              offset: [0, -1]
+            }
+          },
+          {
+            name: 'toggleClass',
+            enabled: true,
+            phase: 'write',
+            fn ({state}) {
+              component.$el.classList.toggle('drop-up', state.placement === 'top')
+            },
+          }]
+      });
+
+      /**
+       * To prevent memory leaks Popper needs to be destroyed.
+       * If you return function, it will be called just before dropdown is removed from DOM.
+       */
+      return () => popper.destroy();
+    }
   }
 };
 </script>
